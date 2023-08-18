d2b12b650997851b04c0d5352924d3b9262fbe13
oralcoder <oralcoder@gmail.com>
2023-08-18 16:18:55 +0900

    code for issue #1

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,8 @@ module.exports = {
     CheckAuth: async function(req, res, next) {
         if(req.isAuthenticated()) {
             console.log('aaa');
+            console.log('aaa');
+            console.log('aaa');
             next();
         }
         else {
